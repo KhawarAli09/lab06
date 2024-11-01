0fddede6bb3323bbf0fc1721ac244de4c0c8e7a7
khawarAli09 <khawarali5491@gmail.com>
2024-11-01 23:26:05 +0500

    first commit

---
+++ task4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+   
+    
+   int j=3;
+    for(int i=1;i<=j;i++)
+    {
+        cout<<" week "<<i<<":"<<endl;
+       int k=1;
+       for(int k=1;k<=7;k++)
+       {
+        cout<<" day "<<k<<":"<<endl;
+        
+       } cout<<endl;
+    
+    }
+
+    }
